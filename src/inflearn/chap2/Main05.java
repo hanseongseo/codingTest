08e335bdd4bbf28c19fb0719b89183f2de063684
hanseongseo <seeman94@naver.com>
2022-10-08 13:49:57 +0900

    소수 판별 문제 에라토스테네스 체로 효율성 해결

--- src/inflearn/chap2/Main05.java
+++ src/inflearn/chap2/Main05.java
@@ -1,33 +1,27 @@
 package inflearn.chap2;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main05 {
+
+    // 에라토스테네스 체
+    public static int primeNumCount(int n) {
+        int answer = 0;
+        int[] ch = new int[n + 1];
+        for (int i = 2; i < ch.length; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j < ch.length; j = j + i) ch[j] = 1;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        int count = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-        List<Integer> primeNumbers = new ArrayList<>();
-        for (int i = 2; i < count; i++) {
-            List<Integer> divisors = new ArrayList<>();
-            Inner : for (int j = 1; j < i; j++) {
-                if (i % j == 0) {
-                    divisors.add(j);
-                } else if (divisors.size() > 1) {
-                    break Inner;
-                }
-            }
-            if (divisors.size() < 2) {
-                primeNumbers.add(i);
-            }
-        }
-
-        primeNumbers.remove(0);
-        bufferedWriter.write(String.valueOf(primeNumbers.size()));
-        bufferedWriter.flush();
-
+        System.out.println(Main05.primeNumCount(n));
     }
 }
